23a641c12cb9732eb9d19eee123083d3b507fcee
LickevicVL <19941510metalhead@gmail.com>
2016-07-30 01:37:34 +0300

    vk.py: likes Sergey's and Valery's friends posts

--- vk.py
+++ vk.py
@@ -1,72 +1,56 @@
 import vk_api
-from random import randint, choice
+from  random import shuffle
+from multiprocessing.dummy import Pool as ThreadPool
+import multiprocessing as mp
+from time import sleep
+
+def captcha_handler(captcha):
+
+    key = ' '
+    # key = input("Enter Captcha {0}: ".format(captcha.get_url())).strip()
+    print(key)
+
+    return captcha.try_again(key)
+
+people = [50264756, 9375156]
+
+login, password = '+375292082080', '1J2345S6789o0Pacan123N45s6t7A8h9A0m'
+vk_session = vk_api.VkApi(login, password, api_version='5.53', captcha_handler=captcha_handler)
+
+try:
+    vk_session.authorization()
+except vk_api.AuthorizationError as error_msg:
+    print(error_msg)
 
 
-def main():
-    """ Пример отправки Jsony Stathomy рандомное сообщенько и возврата id друзей профиля"""
+vk = vk_session.get_api()
 
-    login = input('Введи логин: ')
-    password = input('Введи пароль: ')
-    vk_session = vk_api.VkApi(login, password, api_version='5.53')
 
+with vk_api.VkRequestsPool(vk_session) as pool:
+    person_friends = pool.method_one_param('friends.get', key='user_id', values=people)
+
+
+friends = list(set().union(*(i['items'] for i in person_friends.values())))
+
+def like_last_post(owner_id):
     try:
-        vk_session.authorization()
-    except vk_api.AuthorizationError as error_msg:
-        print(error_msg)
-        return
+        post = vk.wall.get(owner_id=owner_id, count=1)['items']
+        if post:
+            post_id = post[0]['id']
+            vk.likes.add(owner_id=owner_id, item_id=post_id, type='post')
+            print('Поставил лайк юсеру {}'.format(owner_id))
+        else:
+            pass
+            print('Нет записей у юсера {}'.format(owner_id))
+    except:
+        print('Капча или юхер удалён', owner_id)
+        sleep(60)
 
-    """
-    Примеры можно посмотреть здесь: https://github.com/python273/vk_api/tree/master/examples
-    VK API - здесь: https://new.vk.com/dev.php?method=methods
-    """
-    vk = vk_session.get_api()
+shuffle(friends)
 
-    user = vk.users.get()[-1]
+pool = ThreadPool(mp.cpu_count())
+pool.map(like_last_post, friends)
+pool.close()
+pool.join()
 
-    print(user)
 
-    '''Просто способ работы с api
-    '''
-
-    vk.messages.send(user_id='376365095', message='HI LOLKA')
-    print(vk.friends.get(user_id=user['id'])['items'])
-
-    print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
-
-    '''Далее рабочий код, но он работает после подправки файла vk_tools.py (259 строка):
-    for x in range(len(response)):
-                if self.one_param:
-                    if response[x] is False:
-                        self.one_param['return'][cur_pool[x]] = {'_error': True}
-                    else:
-                        self.one_param['return'][cur_pool[x]] = response[x]
-                else:
-                    if response[x] is False:
-                        self.pool[i + x][2].update({'_error': True})
-                    elif type(response[x]) is list:
-                        self.pool[i + x][2].update({'list': response[x]})
-                    else:
-                        if type(response[0]) is int:
-                            self.pool[i + x][2].update()
-                        else:
-                            self.pool[i + x][2].update(response[x])
-    '''
-
-    with vk_api.VkRequestsPool(vk_session) as pool:
-        message = ''
-        for i in range(randint(1, 10)):
-            message += choice(['Syka', 'bliat', 'соси кирпичь', 'улыбашка', 'ti', 'жОпа', 'mamky ipal'])\
-                    + choice([' ', ', '])
-
-        pool.method('messages.send', {'oauth': '1', 'user_id': '376365095', 'message': message})
-
-    with vk_api.VkRequestsPool(vk_session) as pool:
-        friends = pool.method('friends.get', {'user_id': user['id']})
-
-    if friends['items']:
-        print(friends['items'])
-    else:
-        print('Ti odinokaia sychka')
-
-if __name__ == '__main__':
-    main()
